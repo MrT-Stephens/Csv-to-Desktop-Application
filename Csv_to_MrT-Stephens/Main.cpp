b344004403d63136f29be6fa8820311ef040d4ce
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2023-08-16 09:09:15 +0100

    Updated code!

--- Csv_to_MrT-Stephens/Main.cpp
+++ Csv_to_MrT-Stephens/Main.cpp
@@ -7,9 +7,7 @@
 // Global variables for the application
 const std::string mrtApp::APP_NAME = "CSV to...";
 const std::string mrtApp::APP_VERSION = "v0.1.0";
-
 const std::string mrtApp::APP_TITLE = mrtApp::APP_NAME + " " + mrtApp::APP_VERSION;
-
 const std::string mrtApp::GITHUB_URL = "https://github.com/MrT-Stephens";
 
   /**************************/
